ade30d6243d47520cb113797752dc8a9eb8801fd
MeghTarwadi <139316492+MeghTarwadi@users.noreply.github.com>
2023-09-28 14:13:04 +0530

    Add files via upload

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,4 @@
-  // malloc
-    printf( "\nUsing malloc" );
-    ptr = malloc(n * sizeof(int));
-    if (ptr == NULL)
-    {
-        printf("Memory allocation failed. Exiting...\n");
-        return 1;
-    }
-    print(ptr, n);
-    scan(ptr, n);
-    print(ptr, n);
-    free(ptr);
-    print(ptr, n);
+int main()
+// {
+//     func2();
+// }
