57a484d635715c97de80d3e89ee274b374f9a7a2
MeghTarwadi <139316492+MeghTarwadi@users.noreply.github.com>
2024-07-30 15:40:55 +0530

    Add files via upload

--- sort.c
+++ sort.c
@@ -3,29 +3,45 @@
 
 int main() {
     int a[10];
-    double time;
-    clock_t start, end;
+    float time,time1;
+    clock_t start, end,start1,end1;
 
     printf("\n\n=======================================================\n");
-    printf("Enter 10 Digits:")
+    printf("Enter 10 Digits:");
     printf("\n=======================================================\n");
     
     for(int i=0;i<10;i++){
         printf("Enter %d th element : ",i+1);
         scanf("%d", &a[i]);
     }
-
-
+    start1=clock();
+    for(int i=0;i<10;i++){
         start = clock();
-        double ans=1;
-        for(double i=1;i<=a;i++){
-            ans=ans*i;
+        int max = i;
+        
+        for (int j=i;j<10;j++){
+            if(a[max]>a[j]){
+                max = j;
+            }
         }
+
+        int temp=a[i];
+        a[i] = a[max];
+        a[max] = temp;
+
         end = clock();
-        time = (double)(end - start) / CLOCKS_PER_SEC;
-        printf("\nFor %d th pass taken time is %ln", i,time);
+        time = (float)(end - start) / CLOCKS_PER_SEC;
+        printf("\nFor %d th pass taken time is %f",i,time);
+    }
+    end1=clock();
 
+    printf("\n\n=======================================================\n");
 
+    time1 = (float)(end1 - start1) / CLOCKS_PER_SEC;
+    printf("Total time is %f \nSorted array : ",time1);
+    for(int i=0;i<10;i++){
+        printf("%d ",a[i]);
+    }
     printf("\n=======================================================\n\n\n");
 
     return 0;
